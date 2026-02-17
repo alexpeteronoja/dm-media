7891098a29e0ca6bac4ddd71e9936475a9d3474a
unknown <alexpeteronoja@gmail.com>
2026-02-17 18:12:59 +0100

    edited footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,7 @@ function Footer() {
   return (
     <>
       <div className="bg-[#2D2D2D]  px-10 py-8 flex flex-col md:flex-row gap-y-2 items-center md:justify-between">
-        <div className="text-[#FFFFFF] text-sm">
-          © 2023 Anima’s Landing Page Ui Kit.
-        </div>
+        <div className="text-[#FFFFFF] text-sm">© 2026 DM Media.</div>
 
         <div className="text-white flex gap-x-4">
           <CiYoutube />
